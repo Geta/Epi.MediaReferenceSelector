0ef8b493fa28137605481fcdae0e1118ee63befe
Mattias Olsson <mattias@geta.no>
2016-10-27 15:09:14 +0200

    First commit of code.

---
+++ Geta.Epi.MediaReferenceSelector/EditorDescriptors/ImageReferenceEditorDescriptor.cs
@@ -0,0 +1,14 @@
+﻿using EPiServer.Core;
+using EPiServer.Shell.ObjectEditing.EditorDescriptors;
+using EPiServer.Web;
+
+namespace Geta.Epi.MediaReferenceSelector.EditorDescriptors
+{
+	[EditorDescriptorRegistration(TargetType = typeof(ContentReference), EditorDescriptorBehavior = EditorDescriptorBehavior.OverrideDefault, UIHint = UIHint.Image)]
+	public  class ImageReferenceEditorDescriptor: MediaReferenceEditorDescriptorBase<IContentImage>
+	{
+	}
+}
+
+
+
---
+++ Geta.Epi.MediaReferenceSelector/EditorDescriptors/MediaReferenceEditorDescriptorBase.cs
@@ -0,0 +1,16 @@
+﻿using EPiServer.Cms.Shell.UI.UIDescriptors;
+using EPiServer.Core;
+using EPiServer.Shell.ObjectEditing.EditorDescriptors;
+
+namespace Geta.Epi.MediaReferenceSelector.EditorDescriptors
+{
+	public abstract class MediaReferenceEditorDescriptorBase<T> : ContentReferenceEditorDescriptor<T> where T: IContentData
+	{
+		protected MediaReferenceEditorDescriptorBase()
+		{
+			this.ClientEditingClass = "geta-epi-mediareferenceselector/MediaSelector";
+		}
+
+		public override string RepositoryKey { get { return MediaRepositoryDescriptor.RepositoryKey; } }
+	}
+}
